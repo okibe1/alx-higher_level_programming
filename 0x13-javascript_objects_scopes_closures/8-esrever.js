a724bb38e7a14bc1e2f753cdc28b52b00f7a8813
okibe1 <okibe4obinna@gmail.com>
2023-04-14 21:38:24 -0700

    made correction to file

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,19 @@
 #!/usr/bin/node
-// function that returns the reversed version of a list
-exports.esrever = function (list) {
-	const newList = [];
-	for (let i = list.length - 1; i >= 0; i--) {
-		newList.push(list[1]);
-	}
-	return newlist;
+
+/**
+ * A function that returns a reversed version of a given list.
+ * @param {Array} list - the list to reverse.
+ * @returns {Array} - the reversed version of the input list.
+ */
+exports.esrever = function(list) {
+  // Create an empty array to hold the reversed elements
+  const newList = [];
+
+  // Loop through the original list backwards and add each element to the new list
+  for (let i = list.length - 1; i >= 0; i--) {
+    newList.push(list[i]);
+  }
+
+  // Return the reversed list
+  return newList;
 };
