3a64f5f714fd3feb531909a9fbe46346f8d8a102
okibe1 <okibe4obinna2gmail.com>
2023-05-03 04:55:34 +0000

    made changes

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,11 @@
-#!/usr/bin/python3
-# Python script that fetches https://intranet.hbtn.io/status
-import urllib.request
+mport urllib.request
 
-if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as res:
-        content = res.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+url = 'https://alx-intranet.hbtn.io/status'
+
+with urllib.request.urlopen(url) as response:
+    body = response.read()
+
+print("Body response:")
+print("\t- type: {}".format(type(body)))
+print("\t- content: {}".format(body))
+print("\t- utf8 content: {}".format(body.decode('utf-8')))
